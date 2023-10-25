147880b355d2dc3dba18b57c247708d3287345d2
applet <celbever@naver.com>
2023-10-25 10:39:46 +0900

    [master] 기본이 닫힌 상태이도록 css 수정 및 클릭 가능한 엘리먼트 전부 클릭 애니메이션 반영

--- util/closable-container-generator.js
+++ util/closable-container-generator.js
@@ -1,13 +1,14 @@
 export function createClosableContainer($dom, text) {
     const $hideBtn = document.createElement('div');
     $hideBtn.innerText = text;
-    $hideBtn.classList.add('font-bold');
+    $hideBtn.classList.add('font-bold', 'clickable');
     $hideBtn.addEventListener('click', () => {
         $contentWrapper.classList.toggle('hide');
     })
 
     const $contentWrapper = document.createElement('div');
     $contentWrapper.appendChild($dom);
+    $contentWrapper.classList.add('hide')
 
     const $container = document.createElement('div');
     $container.appendChild($hideBtn);
